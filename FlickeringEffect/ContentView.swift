a902511d5822f0bd47787cf81bacb7a1e0ddcb36
Elaidzha1940 <eli080293@gmail.com>
2024-08-10 13:42:03 +0300

    FlickeringEffect

---
+++ FlickeringEffect/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: FlickeringEffect
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.08.2024
+//
+//  /*
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickeringEffect/FlickeringEffectApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: FlickeringEffect
+//  File: FlickeringEffectApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.08.2024
+//
+//  /*
+
+import SwiftUI
+
+@main
+struct FlickeringEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
