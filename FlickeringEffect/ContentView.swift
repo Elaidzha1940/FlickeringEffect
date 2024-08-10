161ac351f29974f4216a0555af0bab0ccb016159
Elaidzha1940 <eli080293@gmail.com>
2024-08-10 20:35:09 +0300

    Update

--- FlickeringEffect/ContentView.swift
+++ FlickeringEffect/ContentView.swift
@@ -12,7 +12,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-          
+          Text("Hooo")
+                .foregroundStyle(.mint)
         }
     }
 }
